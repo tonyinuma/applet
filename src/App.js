96c9c00cbccfe8b7c7bc3e000d2ac59c10180a72
Tony ID <tony.inuma@gmail.com>
2020-04-19 17:07:41 -0500

    [FEAT] Example View List of Tasks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import './App.css';
+import tasks from './sample/tasks.json';
+import Tasks from './components/Tasks';
 
-/* function HelloWorld(props){
-  console.log(props)
-  return(
-    <div id="hello">{props.text}</div>
-  );
-} */
-
-class HelloWorld extends React.Component{
+class App extends Component {
 
   state = {
-    show : true
-  }
-
-  changeState = () => {
-    this.setState({show: !this.state.show})
+    tasks : tasks
   }
 
   render(){
-    if(this.state.show){
-      return(
-        <div className="house">
-          {this.props.text}
-          <button onClick={this.changeState}>Apagar Luz</button>
-        </div>
-        
-      );
-    }else{
-      return <h1 className="house">Luz apagada<button onClick={this.changeState}>Encender Luz</button></h1>
-    }
+    return <div id="main"> 
+      <p id="title" align="center">App For List Tasks</p>  
+      <Tasks tasks={this.state.tasks}/>
+    </div>
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Control de luces de una casa inteligente:
-        <HelloWorld text="Luz sala"/>
-        <HelloWorld text="Luz Cocina"/>
-        <HelloWorld text="Luz Cochera"/>
-        </h1>
-    </div>
-  );
-}
-
 export default App;
+
